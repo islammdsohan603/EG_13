b06d4d8dacaf391df0c6889871e04c320c28abf8
islammdsohan603 <islammdsohan603@gmail.com>
2026-08-21 17:11:56 +0600

    findMode

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -1,5 +1,20 @@
-function naturalSort(arr) {
-  return arr.sort((a, b) => a.localeCompare(b, undefined, { numeric: true }));
+function findMode(arr) {
+  const map = new Map();
+
+  let mode = arr[0];
+  let maxCount = 0;
+
+  for (const item of arr) {
+    const count = (map.get(item) || 0) + 1;
+    map.set(item, count);
+
+    if (count > maxCount) {
+      maxCount = count;
+      mode = item;
+    }
+  }
+
+  return mode;
 }
 
-console.log(naturalSort(["file10.txt", "file2.txt", "file1.txt"]));
+console.log(findMode([1, 3, 3, 2, 1, 3, 4]));
